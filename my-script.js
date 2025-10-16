494d3a682f5ae00471eae38cd562b9432839dce7
Amtoni <dsw52717@student.dsw.edu.pl>
2025-10-17 01:28:59 +0200

    V2

--- my-script.js
+++ my-script.js
@@ -1,12 +1,14 @@
 console.log("Hello, World!");
 
-let size;
-const alert = document.getElementById("alert");
+const alertContainer = document.getElementById("alert");
+const draw = document.getElementById("draw");
+const contener = document.getElementById("grid-container");
+const sizeInput = document.getElementById("size-container");
+const resetButton = document.getElementById("resetButton");
 
-function start(size) {
-  const contener = document.getElementById("grid-container");
+function start() {
   contener.innerHTML = "";
-  size = document.getElementById("size-container").value;
+  const size = sizeInput.value;
 
   if (size <= 100 && size >= 2) {
     console.log("prawidłowa liczba");
@@ -19,15 +21,58 @@ function start(size) {
       contener.appendChild(square);
 
       square.addEventListener("mouseover", (event) => {
-        square.style.backgroundColor = "grey";
+        square.classList.add("coloured-square");
       });
     }
-    alert.textContent = "";
+    alertContainer.textContent = "";
   } else {
     console.log("Wpisz liczbę z przedziału od 2 do 100");
-    alert.textContent = "Pick a number from 2 to 100";
+    alertContainer.textContent = "Pick a number from 2 to 100";
   }
 }
+
+resetButton.addEventListener("click", () => {
+  const squares = Array.from(
+    contener.getElementsByClassName("coloured-square")
+  );
+  console.log(squares);
+  if (squares.length > 1) {
+    squares.forEach((square) => {
+      square.classList.remove("coloured-square");
+    });
+  }
+});
+
+/*
+const size = 5
+<div class='grid-square'></div>
+
+
+<div id="grid-container">
+
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+<div class='grid-square'></div>
+
+
+
+
+</div>
+
+i = 0     i < 25  i++
+i = 1
+
+
+*/
+
+draw.addEventListener("click", start);
 
 draw.addEventListener("mouseover", (event) => {
   draw.style.backgroundColor = "yellow";
